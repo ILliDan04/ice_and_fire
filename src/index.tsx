6c4bc7127d405869758eb9a81a4eb7ffce568f5c
ilyadubov2004@gmail.com <ilyadubov2004@gmail.com>
2022-08-19 22:44:26 +0200

    deploying issues fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "./redux";
 
 const root = ReactDOM.createRoot(
@@ -13,9 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
